ea91b20629fa301f914fd9f398f08562663a289b
Debang chowdhury <sambhuchowdhury512@gmail.com>
2025-07-14 22:28:20 +0530

    commit

---
+++ Variables & Declarations/script1.js
@@ -0,0 +1,7 @@
+const name = "Debang Chowdhury";
+let city = "Kolkata";
+const age = 25;
+
+console.log(name); // Debang Chowdhury
+console.log(city); // Kolkata
+console.log(age); // 25
---
+++ Variables & Declarations/script2.js
@@ -0,0 +1,3 @@
+let x = 10;
+let x = 8;
+console.log(x); // incorrect, will throw an error: Identifier 'x' has already been declared
